063422b7c8dcc27a2eb14ef3cc5a9447172d3dae
Olivia Ifebuche <oliviaifebuche002@gmail.com>
2026-01-10 23:21:02 -0800

    new commit of today

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-let second = 0;
-let minute = 0;
-let hour = 0;
-let time = new Date();
+let hour = document.getElementById('hour-hand');
+let minute = document.getElementById('minute-hand');
+let seconds = document.getElementById('seconds-hand');
+
+function showTheTime() {
+    let date = new Date();
+
+    let hours = date.getHours();
+    let mins = date.getMinutes();
+    let secs = date.getSeconds();
+
+    let hourRotation = 30*hours + mins/2;
+    let minuteRotation = 6*mins;
+    let secondsRotation = 6*secs;
+
+    hour.style.transform = `rotate(${hourRotation}deg)`;
+    minute.style.transform = `rotate(${minuteRotation}deg)`;
+    seconds.style.transform = `rotate(${secondsRotation}deg)`;
+    
+}
+showTheTime();
+
+setInterval(showTheTime,1000);
